08ec0a6326869ae7d4dd9763a68386508ea64fe0
gbear1984 <46852218+gbear1984@users.noreply.github.com>
2019-01-30 17:48:22 -0600

    Update shaderBaker.py

--- shaderBaker.py
+++ shaderBaker.py
@@ -3,20 +3,24 @@ import maya.cmds as cmds
 import os
 
 
-#selection = cmds.ls(sl=True, long=True) #or [] 
-shaderTree = cmds.listConnections(c=True)
-#for i in shaderTree:
-#    print (i)
+#selection = cmds.ls(sl=True, long=True) #or []
+#need to test for mtl before running filter
+def filterSelection():
+    shaderTree = cmds.listConnections(d=True)
+    shaderTree.remove("materialInfo1")
+    shaderTree.remove("defaultShaderList1")
+    return shaderTree
+    #for i in shaderTree:
+    #    print (i)
         
 def makeShadeAndBakeGUI_SE():
-    
     selection = cmds.ls(sl=True, long=True)
     if (cmds.window("shadeAndBakeWindowSE", exists=True)):
         cmds.deleteUI("shadeAndBakeWindowSE")
     
     shadeAndBakeWindowSE = cmds.window('shadeAndBakeWindowSE', title="Shader Baking", rtf=True) 
     cmds.paneLayout() 
-    scrollList = cmds.textScrollList("cunt",numberOfRows=8, allowMultiSelection=True, append= shaderTree)
+    scrollList = cmds.textScrollList("Shader Connects",numberOfRows=2, allowMultiSelection=True, append= filterSelection())
 
     cmds.showWindow('shadeAndBakeWindowSE')
 makeShadeAndBakeGUI_SE()
